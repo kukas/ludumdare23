ee749d201c7ea760f2b03b7526ee1fd9d402d210
Stepan <stepanmarek@centrum.cz>
2012-04-22 16:16:57 +0200

    Řešení bugu s kamerou a vzájemným pohybem tříd.

--- entities.js
+++ entities.js
@@ -2,6 +2,8 @@ function Warbot(x,y){
 	this.x=x;
 	this.y=y;
 	this.health=20;
+	this.target={x:0,y:0};
+	this.vector={x:0,y:0};
 	this.texture=new Animation("Assets/warbot/warbot.png",3,5);
 };
 Warbot.prototype=new Nanobot();
@@ -10,6 +12,8 @@ function Builder(x,y){
 	this.x=x;
 	this.y=y;
 	this.health=5;
+	this.target={x:0,y:0};
+	this.vector={x:0,y:0};
 	this.texture= new Animation("Assets/builder/builder.png",4,1)
 };
 Builder.prototype=new Nanobot();
@@ -18,6 +22,8 @@ function Enginner(x,y){
 	this.x=x;
 	this.y=y;
 	this.health=5;
+	this.target={x:0,y:0};
+	this.vector={x:0,y:0};
 	this.texture=new Animation("Assets/enginner.png",3,1);
 };
 Enginner.prototype=new Nanobot();
