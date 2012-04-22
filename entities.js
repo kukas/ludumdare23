43e8e2b96bc1c1839ab9a59eb00833db73a58c34
Stepan <stepanmarek@centrum.cz>
2012-04-22 15:22:24 +0200

    Zobjektivnění pro dobrou dědičnost mezi objetky.

---
+++ entities.js
@@ -0,0 +1,23 @@
+function Warbot(x,y){
+	this.x=x;
+	this.y=y;
+	this.health=20;
+	this.texture=new Animation("Assets/warbot/warbot.png",3,5);
+};
+Warbot.prototype=new Nanobot();
+
+function Builder(x,y){
+	this.x=x;
+	this.y=y;
+	this.health=5;
+	this.texture= new Animation("Assets/builder/builder.png",4,1)
+};
+Builder.prototype=new Nanobot();
+
+function Enginner(x,y){
+	this.x=x;
+	this.y=y;
+	this.health=5;
+	this.texture=new Animation("Assets/enginner.png",3,1);
+};
+Enginner.prototype=new Nanobot();
