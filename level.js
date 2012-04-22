3ad5c04338f6be22c06b672066bc08e53e8b191f
Stepan <stepanmarek@centrum.cz>
2012-04-22 15:29:26 +0200

    Přidání několika testovacích objektů.

--- level.js
+++ level.js
@@ -1,4 +1,4 @@
 var level = {
-	objects : [ new Cell(800,480), new Builder(400,300) ],
+	objects : [ new Cell(800,480), new Warbot(400,300) , new Builder(300,500), new Enginner(600,300)],
 	background : "Assets/background1.png",
 }
